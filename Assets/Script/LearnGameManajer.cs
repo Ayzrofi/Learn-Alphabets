e20dc4141abb9940c7b8b0aed447534faf9c49eb
Ayzrofi <45961870+Ayzrofi@users.noreply.github.com>
2018-12-23 18:14:20 +0700

    Initial commit

---
+++ Assets/Script/LearnGameManajer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class LearnGameManajer : MonoBehaviour {
+    [SerializeField]
+    private AudioSource AudioSrc;
+
+    private void Awake()
+    {
+        if (AudioSrc == null)
+            AudioSrc = GetComponent<AudioSource>();
+    }
+}
